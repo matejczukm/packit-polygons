ca2e5f51efba17e36031b9b0319f5a50f0cd315c
piotr-kosakowski <piotrkosakowski@icloud.com>
2025-01-04 18:56:35 +0100

    replace game logic code with package import

--- djangoProject/packitPolygons/views.py
+++ djangoProject/packitPolygons/views.py
@@ -1,32 +1,9 @@
-import datetime
-
-import numpy as np
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 import json
-import sys
-import os
 from .models import Game
-# from mcts_simple import UCT, MCTS
-import random
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PARENT_DIR = os.path.abspath(os.path.join(BASE_DIR, '..'))
-if PARENT_DIR not in sys.path:
-    sys.path.append(PARENT_DIR)
-# from our_packit.triangular_mode import frontend_interface as tri_fi
-# from our_packit.hexagonal_mode import frontend_interface as hex_fi
-# from our_packit.triangular_mode import data_conversions as tri_dc
-# from our_packit.hexagonal_mode import data_conversions as hex_dc
-#
-# sys.path.insert(0, './alpha-zero-general')
-# from PackitAIPlayer import AIPlayer
-#
-# # ai = AIPlayer(4, 'triangular')
-# ai_players = {}
-
-from our_packit import frontend_interface as packit_fi
+import polygonal_packit.game_core.frontend_interface as packit
 
 
 def hexagon(request):
@@ -58,7 +35,7 @@ def start_new_game(request):
     if request.method == 'POST':
         data = json.loads(request.body)
         return JsonResponse(
-            packit_fi.start_new_game(data['board_size'], data['game_mode'], data['ai_mode'], data['ai_starts'])
+            packit.start_new_game(data['board_size'], data['game_mode'], data['ai_mode'], data['ai_starts'])
         )
 
 
@@ -67,5 +44,5 @@ def confirm_move(request):
     if request.method == 'POST':
         data = json.loads(request.body)
         return JsonResponse(
-            packit_fi.confirm_move(data['board'], data['move'], data['turn'], data['game_mode'], data['ai_mode'])
+            packit.confirm_move(data['board'], data['move'], data['turn'], data['game_mode'], data['ai_mode'])
         )
--- our_packit/triangular_mode/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import game_logic
-from . import data_conversions
-from . import polygon_creation
-from . import size_modifications
-from . import display
