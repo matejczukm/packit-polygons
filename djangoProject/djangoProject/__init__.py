e4e4047c2e45eca5e4a21356107dc462b6b73d58
piotr-kosakowski <piotrkosakowski@icloud.com>
2025-01-27 02:54:27 +0100

    .

--- djangoProject/djangoProject/__init__.py
+++ djangoProject/djangoProject/__init__.py
@@ -3,18 +3,18 @@ import webbrowser
 from django.core.management import call_command
 from django.core.management.utils import get_random_secret_key
 import django
-# from ..packitPolygons.ai_players_registry import AIPlayerRegistry
+from ..packitPolygons.ai_players_registry import AIPlayerRegistry
 
 
 def run_app(ai_players_dict=None, open_browser=True, port=8000):
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoProject.djangoProject.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoProject.settings")
 
     # secret_key = os.environ.get("DJANGO_SECRET_KEY", get_random_secret_key())
     # os.environ["DJANGO_SECRET_KEY"] = secret_key
 
     django.setup()
-    # for name, player in ai_players_dict.items():
-    #     AIPlayerRegistry.add_player(name, player)
+    for name, player in ai_players_dict.items():
+        AIPlayerRegistry.add_player(name, player)
 
     if open_browser:
         webbrowser.open(f"http://127.0.0.1:{port}")
